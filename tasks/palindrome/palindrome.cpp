93cf4cb2c23ef86f20b95195166a9852cc00385f
Igor Avdeenko <jagdkotehok@gmail.com>
2022-09-15 22:03:13 +0300

    Added solve

--- tasks/palindrome/palindrome.cpp
+++ tasks/palindrome/palindrome.cpp
@@ -1,5 +1,29 @@
 #include "palindrome.h"
 
+const int SPACE_CODE = static_cast<int>(' ');
+
 bool IsPalindrome(const std::string& str) {
-    return false;
+    if (str.empty()) {
+        return true;
+    }
+    size_t left_index = 0;
+    size_t right_index = str.size() - 1;
+    while (left_index < right_index) {
+        int left_symbol = static_cast<int>(str[left_index]);
+        int right_symbol = static_cast<int>(str[right_index]);
+        if (left_symbol == SPACE_CODE) {
+            ++left_index;
+            continue;
+        }
+        if (right_symbol == SPACE_CODE) {
+            --right_index;
+            continue;
+        }
+        if (left_symbol != right_symbol) {
+            return false;
+        }
+        ++left_index;
+        --right_index;
+    }
+    return true;
 }
