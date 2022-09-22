05c3be6dbcf9bd3a0131651b32456910bf14fe8b
Igor Avdeenko <jagdkotehok@gmail.com>
2022-09-22 16:15:28 +0300

    fixed solve

--- tasks/word2vec/word2vec.cpp
+++ tasks/word2vec/word2vec.cpp
@@ -1,10 +1,11 @@
 #include "word2vec.h"
 
 #include <vector>
+#include <limits>
 
 std::vector<std::string> FindClosestWords(const std::vector<std::string>& words,
                                           const std::vector<std::vector<int>>& vectors) {
-    int max_word_value = -INT_MAX;
+    int max_word_value = std::numeric_limits<int>::min();
     for (size_t i = 1; i < vectors.size(); ++i) {
         int current_word_value = 0;
         for (size_t j = 0; j < vectors[i].size(); ++j) {
