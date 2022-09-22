6fcce345e9b699f0e6920dc93cf8b2e0ec361f05
Igor Avdeenko <jagdkotehok@gmail.com>
2022-09-22 13:17:17 +0300

    Added solve

--- tasks/admission/admission.cpp
+++ tasks/admission/admission.cpp
@@ -1,5 +1,35 @@
 #include "admission.h"
 
+bool operator < (const Date& l, const Date& r) {
+    return std::tie(l.year, l.month, l.day) < std::tie(r.year, r.month, r.day);
+}
+
 AdmissionTable FillUniversities(const std::vector<University>& universities, const std::vector<Applicant>& applicants) {
-    return {};
+    std::vector<const Applicant*> applicants_pointers;
+    for (const Applicant& applicant : applicants) {
+        applicants_pointers.emplace_back(&applicant);
+    }
+    std::sort(applicants_pointers.begin(), applicants_pointers.end(), [&](const Applicant* l, const Applicant* r){
+        if (l->points != r->points) {
+            return l->points > r->points;
+        } else {
+            return std::tie(l->student.birth_date, l->student.name) < std::tie(r->student.birth_date, r->student.name);
+        }
+    });
+    std::unordered_map<std::string, size_t> university_students_left;
+    for (const University& university : universities)  {
+        university_students_left[university.name] = university.max_students;
+    }
+    AdmissionTable admission_table = {};
+    for (const Applicant* applicant : applicants_pointers) {
+        for (const std::string& university : applicant->wish_list) {
+            size_t& unused_places = university_students_left[university];
+            if (unused_places > 0) {
+                --unused_places;
+                admission_table[university].emplace_back(&applicant->student);
+                break;
+            }
+        }
+    }
+    return admission_table;
 }
