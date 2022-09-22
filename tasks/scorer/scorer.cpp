c2e407bd41fab5ec2a575e5961d20c53cc90dcf0
Igor Avdeenko <jagdkotehok@gmail.com>
2022-09-22 13:57:22 +0300

    Fixed problem with ignoring merge_request state

--- tasks/scorer/scorer.cpp
+++ tasks/scorer/scorer.cpp
@@ -12,16 +12,16 @@ ScoreTable GetScoredStudents(const Events& events, time_t score_time) {
     std::unordered_map<std::string, std::unordered_map<std::string, bool>>
         student_task_state;  /// 0 -- failed   1 -- success
     std::unordered_map<std::string, std::unordered_map<std::string, bool>>
-        student_merge_request_state;  /// 0 -- open   1 -- closed
+        student_merge_request_state;  /// 1 -- open   0 -- closed
     for (const Event* event_pointer : events_pointers) {
         if (event_pointer->time > score_time) {
             break;
         }
         EventType event_type = event_pointer->event_type;
         if (event_type == EventType::MergeRequestOpen) {
-            student_merge_request_state[event_pointer->student_name][event_pointer->task_name] = false;
-        } else if (event_type == EventType::MergeRequestClosed) {
             student_merge_request_state[event_pointer->student_name][event_pointer->task_name] = true;
+        } else if (event_type == EventType::MergeRequestClosed) {
+            student_merge_request_state[event_pointer->student_name][event_pointer->task_name] = false;
         } else if (event_type == EventType::CheckFailed) {
             student_task_state[event_pointer->student_name][event_pointer->task_name] = false;
         } else {
@@ -31,7 +31,7 @@ ScoreTable GetScoredStudents(const Events& events, time_t score_time) {
     ScoreTable score_table = {};
     for (const auto& [student, tasks] : student_task_state) {
         for (const auto& [task, task_state] : tasks) {
-            if (task_state) {
+            if (task_state && !student_merge_request_state[student][task]) { /// task accepted and merge_request closed
                 score_table[student].emplace(task);
             }
         }
