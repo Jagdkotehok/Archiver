001b88a92883a91ec92143dd9a20e65dcb64f077
Igor Avdeenko <jagdkotehok@gmail.com>
2022-10-12 20:22:35 +0300

    added solve

--- tasks/zip/zip.cpp
+++ tasks/zip/zip.cpp
@@ -1,1 +1,40 @@
 #include "zip.h"
+
+Zipped::ZipIterator::ZipIterator(Iterator iter1, Iterator iter2) {
+    iter1_ = iter1;
+    iter2_ = iter2;
+}
+
+ZippedPair Zipped::ZipIterator::operator*() const {
+    return ZippedPair(*iter1_, *iter2_);
+}
+
+Zipped::ZipIterator& Zipped::ZipIterator::operator++() {
+    ++iter1_;
+    ++iter2_;
+    return *this;
+}
+
+bool Zipped::ZipIterator::operator==(const Zipped::ZipIterator& other) const {
+    return (iter1_ == other.iter1_) || (iter2_ == other.iter2_);
+}
+
+bool Zipped::ZipIterator::operator!=(const Zipped::ZipIterator& other) const {
+    return !(*this == other);
+}
+
+Zipped::ZipIterator Zipped::begin() const {
+    return begin_;
+}
+
+Zipped::ZipIterator Zipped::end() const {
+    return end_;
+}
+Zipped::Zipped(Zipped::ZipIterator list_begin, Zipped::ZipIterator list_end) : begin_(list_begin), end_(list_end) {
+}
+
+Zipped Zip(Iterator a_begin, Iterator a_end, Iterator b_begin, Iterator b_end) {
+    Zipped::ZipIterator result_begin(a_begin, b_begin);
+    Zipped::ZipIterator result_end(a_end, b_end);
+    return Zipped(result_begin, result_end);
+}
--- tasks/zip/zip.h
+++ tasks/zip/zip.h
@@ -2,11 +2,36 @@
 
 #include <forward_list>
 #include <string>
+#include <tuple>
 
 using Value = std::string;
-using Iterator = std::forward_list<std::string>::const_iterator;
+using Iterator = std::forward_list<Value>::const_iterator;
 using ZippedPair = std::pair<const Value&, const Value&>;
 
-class Zipped;
+class Zipped {
+public:
+    class ZipIterator {
+    public:
+        Iterator iter1_;
+        Iterator iter2_;
+
+        ZipIterator(Iterator iter1, Iterator iter2);
+
+        ZippedPair operator*() const;
+
+        ZipIterator& operator++();
+
+        bool operator==(const ZipIterator& other) const;
+        bool operator!=(const ZipIterator& other) const;
+    };
+
+    Zipped(ZipIterator list_begin, ZipIterator list_end);
+
+    ZipIterator begin_;
+    ZipIterator end_;
+
+    ZipIterator begin() const;  // NOLINT
+    ZipIterator end() const;    // NOLINT
+};
 
 Zipped Zip(Iterator a_begin, Iterator a_end, Iterator b_begin, Iterator b_end);
